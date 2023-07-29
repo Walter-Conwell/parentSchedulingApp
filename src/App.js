2fae082665b4733ceb0a5a24f7e93117f1587e99
Walter-Conwell <131832544+Walter-Conwell@users.noreply.github.com>
2023-07-29 12:50:15 -0600

    Worked on components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Navbar from "./components/navbar";
 import { Flex, Box, Link, Spacer } from "@chakra-ui/react";
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
@@ -25,25 +25,28 @@ function App() {
   );
 }
 
-function Navbar() {
+function App() {
   return (
-    <Flex bg="blue.500" p="4" justifyContent="space-between">
-      <Box>
-        <Link color="white" href="/">
-          Home
-        </Link>
-      </Box>
-      <Box>
-        <Link color="white" href="/about">
-          About
-        </Link>
-      </Box>
-      <Box>
-        <Link color="white" href="/contact">
-          Contact
-        </Link>
-      </Box>
-    </Flex>
+    <div>
+      <Flex bg="blue.500" p="4" justifyContent="space-between">
+        <Box>
+          <Link color="white" href="/">
+            Home
+          </Link>
+        </Box>
+        <Box>
+          <Link color="white" href="/about">
+            About
+          </Link>
+        </Box>
+        <Box>
+          <Link color="white" href="/contact">
+            Contact
+          </Link>
+        </Box>
+      </Flex>
+    </div>
   );
 }
+// sign in and sign up functions
 export default App;
