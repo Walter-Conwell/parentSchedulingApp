2a4efc9b484c5b48f4f158b53e0c8d0cfaef4b5f
Walter-Conwell <131832544+Walter-Conwell@users.noreply.github.com>
2023-07-30 14:11:35 -0600

    added some basic components, next will develop the remaining static components!

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import React, { useState } from "react";
 import "./App.css";
+import { Flex, Box, Link, Spacer } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("About");
+  const handlePageChange = (page) => setCurrentPage(page);
   return (
     <div className="App">
-      <h1>KidzDirect</h1>
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Main currentPage={currentPage} />
     </div>
+    
   );
 }
 
---
+++ client/src/components/Main.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import About from "./About";
+import Contact from "./Contact";
+
+const Main = ({ currentPage }) => {
+  console.log(currentPage);
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <About />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    return <Contact />;
+  };
+  return <div className="main">{renderPage()}</div>;
+};
+
+export default Main;
