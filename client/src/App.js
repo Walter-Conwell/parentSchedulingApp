fa26de9759dd9d6f4d1d6401e2be6fef893628e1
Walter-Conwell <131832544+Walter-Conwell@users.noreply.github.com>
2023-07-31 20:44:06 -0600

    Added modal functionality! up next, some styling  and more elements!

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,31 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import { Flex, Box, Link, Spacer } from "@chakra-ui/react";
 import { SettingsIcon } from "@chakra-ui/icons";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import SignUp from "./components/Signup";
 import Footer from "./components/Footer";
-import DrawerExample from "./components/Drawer";
+import Contact from "./components/Contact";
+// import DrawerExample from "./components/Drawer";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("About");
-  const handlePageChange = (page) => setCurrentPage(page);
   return (
-    <div className="App">
-      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
-      <DrawerExample
-        currentPage={currentPage}
-        handlePageChange={handlePageChange}
-      />
-      <Main currentPage={currentPage} />
-      <Footer currentPage={currentPage} handlePageChange={handlePageChange} />
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* Routes defined here */}
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
-
+// nav links is similar to this concept
 export default App;
 // DrawerExample identifier should be changed, that was default name from library!
