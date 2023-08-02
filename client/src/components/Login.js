3abcc6e2c78f942f9c8d088200f1b9d9795d0ec2
Kyle <kyleslaughter8@gmail.com>
2023-08-02 16:12:38 -0600

    added login functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,19 +1,37 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
+import logoImage from "../assets/kidzdirect-low-resolution-color-logo.png";
+import { useMutation } from "@apollo/client";
+import { LOGIN_PROFILE } from "../utils/mutations";
+import auth from "../utils/auth";
+import React, { useState } from "react";
 
 
 // // login function
 const Login = () => {
+    const [ login, { error, data } ] = useMutation(LOGIN_PROFILE);
+    if ( error ) {
+        console.log(JSON.stringify(error));
+    }
+
     const [loginStatus, setLoginStatus] = React.useState('Log in')
-    const onSubmit = (e) => {
-        e.preventDefault()
-        setLoginStatus('Logging in...')
+    const onSubmit = async (e) => {
+        e.preventDefault();
         const { email, password } = e.target.elements
         let loginCredentials = {
             email: email.value,
             password: password.value,
         }
         console.log(loginCredentials);
+        try {
+            const { data } = await login({
+              variables: loginCredentials
+            });
+      
+            auth.login(data.login.token);
+      
+            console.log(data);
+          } catch (error) {
+            console.log(JSON.stringify(error));
+          }
     }
     return (<form onSubmit={onSubmit}>
             <div className="row mb-3">
