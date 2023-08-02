cd2571f6f6af1f569e35f5b09ef6b9eef2af66c3
Walter-Conwell <131832544+Walter-Conwell@users.noreply.github.com>
2023-08-02 11:42:08 -0600

    fixed basic structure... dropping CHAKRA UI

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,34 +1,9 @@
-import React, { useState } from "react";
-import "../../src/App.css";
-import { ChakraProvider } from "@chakra-ui/react";
+// import React, { useState } from "react";
+// import "../../src/App.css";
 
-function Login() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+// // login function
 
-  return (
-    <div className="App">
-      <ChakraProvider>
-        <Button onClick={onOpen}>Login</Button>
+// f
 
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Welcome!</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <h1>Sign up with us to start Connecting with Resources!</h1>
-            </ModalBody>
-
-            <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={onClose}>
-                Save
-              </Button>
-              <Button variant="ghost">Close</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </ChakraProvider>
-    </div>
-  );
-}
-export default Login;
+ 
+// export default Login;
